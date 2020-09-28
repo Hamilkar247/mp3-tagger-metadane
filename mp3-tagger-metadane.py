e1385f0c7a4092db2d693b9a6e9350cbcef3b767
Hamilkar247 <adonnibal96@gmail.com>
2020-09-28 20:46:49 +0200

    WO-9 wartości default w argparse

--- mp3-tagger-metadane.py
+++ mp3-tagger-metadane.py
@@ -28,13 +28,13 @@ def def_params():
     parser.add_argument("-f", "--file", help="plik mp3 do tagowania", required=True)
     parser.add_argument("-l", "--loghami", action='store_true'
                         , help="increase output verbosity", required=False)
-    parser.add_argument("-al", "--album", help="tag dla albumu", required=False)
-    parser.add_argument("-ar", "--artist", help="tag dla autora/artysty", required=False)
-    parser.add_argument("-s", "--song", help='tag dla piosenki', required=False)
-    parser.add_argument("-t", "--track", help="tag dla ścieszki", required=False)
-    parser.add_argument("-c", "--comment", help="tag dla komentarzy", required=False)
-    parser.add_argument("-g", "--genre", help="tag dla gatunku muzycznego", required=False)
-    parser.add_argument("-y", "--year", help="tag dla roku utworu", required=False)
+    parser.add_argument("-al", "--album", help="tag dla albumu", required=False, default="")
+    parser.add_argument("-ar", "--artist", help="tag dla autora/artysty", required=False, default="")
+    parser.add_argument("-s", "--song", help='tag dla piosenki', required=False, default="")
+    parser.add_argument("-t", "--track", help="tag dla ścieszki", required=False, default=1)
+    parser.add_argument("-c", "--comment", help="tag dla komentarzy", required=False, default="")
+    parser.add_argument("-g", "--genre", help="tag dla gatunku muzycznego", required=False, default=0)
+    parser.add_argument("-y", "--year", help="tag dla roku utworu", required=False, default="")
 
     args = parser.parse_args()
     if args.loghami:
@@ -51,32 +51,20 @@ def main():
     logging.debug("parametry:" + str(args))
     if args.album is not None:
         mp3.album = args.album
-    else:
-        mp3.album = ""
     if args.artist is not None:
         mp3.artist = args.artist
-    else:
-        mp3.artist = ""
     if args.song is not None:
         mp3.song = args.song
-    else:
-        mp3.song = ""
     if args.track is not None:# track powinno być number nie literal !
         mp3.track = args.track
-    else:
-        mp3.track = "0" #musi być jakaś wartość - dlatego 0
     if args.comment is not None:
         mp3.comment = args.comment
     else:
         mp3.comment = ""
     if args.genre is not None:
         mp3.genre = args.genre #to jest enum de facto - przyjmijmy że wpisujemy 0
-    else:
-        mp3.genre = 0
     if args.year is not None:
         mp3.year = args.year
-    else:
-        mp3.year = ""
     mp3.save()
     pprint(mp3.get_tags())
 
